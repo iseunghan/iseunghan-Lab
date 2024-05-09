3d01aa80b75abd0a43b86e9d574b8f457c507fca
iseunghan <tmdgks301@naver.com>
2024-05-09 22:37:58 +0900

    [effective-java-3] - [item 36] - 비트 필드 대신 EnumSet을 사용하라

---
+++ effective-java-3/src/main/java/me/iseunghan/item36/Wrong_Text.java
@@ -0,0 +1,12 @@
+package me.iseunghan.item36;
+
+public class Wrong_Text {
+    public static final int STYLE_BOLD = 1 << 0;
+    public static final int STYPE_ITALIC = 1 << 1;
+    public static final int STYLE_UNDERLINE = 1 << 2;
+    public static final int STYLE_STRIKETHROUGH = 1 << 3;
+
+    public void applyStyles(int styles) {
+        // do something..
+    }
+}
