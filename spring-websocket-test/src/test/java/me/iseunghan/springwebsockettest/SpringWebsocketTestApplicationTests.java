3472a5b83d41da65d721ae9256498fea9dff88e1
iseunghan <tmdgks301@naver.com>
2024-05-10 23:13:28 +0900

    init project

---
+++ spring-websocket-test/src/test/java/me/iseunghan/springwebsockettest/SpringWebsocketTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.iseunghan.springwebsockettest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebsocketTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
