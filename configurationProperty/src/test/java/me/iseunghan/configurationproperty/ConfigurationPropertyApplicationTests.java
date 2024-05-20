bc16af58cb2ce0a96f5e84a661d10acda343fb26
iseunghan <tmdgks301@naver.com>
2024-05-20 19:33:20 +0900

    [spring/configuration-property] init

---
+++ configurationProperty/src/test/java/me/iseunghan/configurationproperty/ConfigurationPropertyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.iseunghan.configurationproperty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationPropertyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
