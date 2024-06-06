fa36ca621313e0bb1316b276dcd4f726fffa70b1
iseunghan <tmdgks301@naver.com>
2024-06-06 21:53:10 +0900

    [spring/zip-unzip-handling] unZip 기능 개발

--- spring-zip-unzip-handling/src/main/java/me/iseunghan/springzipunziphandling/util/ZipUtils.java
+++ spring-zip-unzip-handling/src/main/java/me/iseunghan/springzipunziphandling/util/ZipUtils.java
@@ -1,10 +1,16 @@
 package me.iseunghan.springzipunziphandling.util;
 
+import jakarta.annotation.Nullable;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Objects;
+import java.util.function.Function;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 import static org.springframework.util.StringUtils.hasText;
@@ -42,8 +48,43 @@ public class ZipUtils {
                 zipOutputStream.closeEntry();
             } catch (FileNotFoundException e) {
                 log.error("File not found: {}, Exception: {}", parentFile, e.getMessage());
+                throw new RuntimeException(e);
             } catch (IOException e) {
                 log.error("Error while zipping file: {}, Exception: {}", parentFile, e.getMessage());
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static void unZipFile(String dstPath, File zipFile) throws IOException {
+        unZipFile(dstPath, new FileInputStream(zipFile), null);
+    }
+
+    public static void unZipFile(String dstPath, InputStream inputStream, @Nullable Function<Path, Path> renameDuplicatedFilename) throws IOException {
+        try (ZipInputStream zipInputStream = new ZipInputStream(inputStream, Charset.forName("EUC-KR"))) {
+            ZipEntry entry;
+            byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+
+            while ((entry = zipInputStream.getNextEntry()) != null) {
+                final String entryName = entry.getName();
+                if (entry.isDirectory() || entryName.startsWith("__MACOSX")) continue;
+
+                File entryFile;
+                if (renameDuplicatedFilename == null) {
+                    entryFile = new File(dstPath, entryName);
+                } else {
+                    Path path = Path.of(dstPath, entryName);
+                    entryFile = renameDuplicatedFilename.apply(path).toFile();
+                }
+                Files.createDirectories(entryFile.getParentFile().toPath());
+
+                try (BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(entryFile))) {
+                    int bytesRead;
+                    while ((bytesRead = zipInputStream.read(buffer)) != -1) {
+                        outputStream.write(buffer, 0, bytesRead);
+                    }
+                }
+                zipInputStream.closeEntry();
             }
         }
     }
