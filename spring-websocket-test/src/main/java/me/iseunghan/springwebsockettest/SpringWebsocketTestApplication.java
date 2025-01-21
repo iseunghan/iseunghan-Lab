506116bef619935f136fe3bdd0ea702c3fa542c7
iseunghan <tmdgks301@naver.com>
2025-01-21 21:48:57 +0900

    [spring/websocket-test] first commit

---
+++ spring-websocket-test/src/main/java/me/iseunghan/springwebsockettest/SpringWebsocketTestApplication.java
@@ -0,0 +1,13 @@
+package me.iseunghan.springwebsockettest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsocketTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebsocketTestApplication.class, args);
+    }
+
+}
