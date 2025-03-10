bd7bf4fdc7c2d95a43d7e0c8322eed5efa67ebf7
iseunghan <tmdgks301@naver.com>
2025-03-10 20:56:14 +0900

    [python/basic-concept] Generator Example 3 (Return)

--- python-basic-concept/generator_examples/send_return_generator.py
+++ python-basic-concept/generator_examples/send_return_generator.py
@@ -6,7 +6,7 @@ def send_message(message: str) -> str:
     print(f"Sending: {message}")
     return str(random.randint(1, 1000))
 
-def chat(message: str) -> Generator[str, str, None]:
+def chat(message: str) -> Generator[str, str, list[str]]:
     print("Starting a new chat")
     history = []
 
@@ -17,7 +17,9 @@ def chat(message: str) -> Generator[str, str, None]:
         history.append(response)
         print("before yield")
         message = yield response
-        print("after yield")
+        print("after yield") # 여기서 부터는 next()를 호출해야 실행된다. (그렇기 때문에 위에서 yield로 지정한 message가 클라이언트 호출 매개변수로 다시 덮어쓰여지게 된다.)
+        if not message:
+            return history
 
 if __name__ == '__main__':
     """
@@ -60,3 +62,16 @@ if __name__ == '__main__':
     print("> Before Use Yield Field")
     print(res3)
     print("< After Use Yield Field")
+
+    try:
+        res4 = quick_chat.send("") # <-- Occur StopIteration Exception + Generator is Finish
+        print("> Before Use Yield Field")
+        print(res4)
+        print("< After Use Yield Field")
+    except StopIteration as e:
+        """
+        [ 추가 콘솔 출력 ]
+        after yield
+        ['hello', '772', 'how are you doing?', '821', 'oh, that is nice!', '741']
+        """
+        print(e.value)
